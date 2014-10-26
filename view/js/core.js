ed7a44216814022d251145d05f217b7383ff9393
kocsen <kocsenc@gmail.com>
2014-10-26 11:22:54 -0400

    - Factory injection working

--- view/js/core.js
+++ view/js/core.js
@@ -7,18 +7,33 @@
 var app = angular.module('PayBookApp', []);
 
 
-app.factory('MainFactory', function ($http) {
+app.factory('MainFactory', ['$http', function ($http) {
     return {
-        apiurl: "https://apiurl"
+        baseurl: "http://localhost:8080",
+
+        test: "/test",
+
+        getUsers: function () {
+            return $http.get(this.baseurl + this.test);
+
+        }
+
     };
 
-});
+}]);
 
 
-var mainController = function ($scope) {
-    $scope.yourName = null;
+app.controller('MainController', ['$scope', 'MainFactory', function ($scope, MainFactory) {
 
+    console.log("running request");
+    console.log(MainFactory.test);
+    MainFactory.getUsers().
+        success(function (data) {
+            $scope.users = data;
+        }).
+        error(function (err) {
+            console.log("ERROR!");
+            console.log(err);
+        });
 
-};
-
-app.controller('MainController', mainController);
+}]);
