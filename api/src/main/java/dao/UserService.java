93c016ce0a34df7142931dfaf4acfd7209bb8d9f
CShapleigh <CShapleigh@gmail.com>
2014-11-09 15:08:28 -0500

    friend request endpoint

--- api/src/main/java/dao/UserService.java
+++ api/src/main/java/dao/UserService.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 
 import utility.DBUtility;
 import domain.User;
+import domain.FriendRequest;
 
 public class UserService {
  
@@ -223,47 +224,47 @@ public ResponseEntity<String> register(User user) throws SQLException{
 }
 
 public List<FriendRequest> getFriendRequests(int id){
-    List<FriendRequest> fReqs = new ArrayList<FriendRequests>();
+    List<FriendRequest> fReqs = new ArrayList<FriendRequest>();
     try {
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery("select * from F_Req where receiver = " + id + " and trust = false");
         while (rs.next()) {
-            int id = rs.getInt("rid");
+            int rid = rs.getInt("rid");
             int sender = rs.getInt("sender");
             int receiver = rs.getInt("receiver");
-            bool trust = false;
-            FriendRequest fReq = new FriendRequest(id, sender, receiver, trust);
-            if (fReqs.contans(fReq) == false) {
+            boolean trust = false;
+            FriendRequest fReq = new FriendRequest(rid, sender, receiver, trust);
+            if (fReqs.contains(fReq) == false) {
                 fReqs.add(fReq);
             }
         }
     } catch (SQLException e) {
         e.printStackTrace();
     }
-    return fReqs();
+    return fReqs;
 
 }
 
 
 public List<FriendRequest> getTrustRequests(int id){
-    List<FriendRequest> tReqs = new ArrayList<FriendRequests>();
+    List<FriendRequest> tReqs = new ArrayList<FriendRequest>();
     try {
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery("select * from F_Req where receiver = " + id + " and trust = true");
         while (rs.next()) {
-            int id = rs.getInt("rid");
+            int rid = rs.getInt("rid");
             int sender = rs.getInt("sender");
             int receiver = rs.getInt("receiver");
-            bool trust = false;
-            FriendRequest fReq = new FriendRequest(id, sender, receiver, trust);
-            if (tReqs.contans(fReq) == false) {
+            boolean trust = false;
+            FriendRequest fReq = new FriendRequest(rid, sender, receiver, trust);
+            if (tReqs.contains(fReq) == false) {
                 tReqs.add(fReq);
             }
         }
     } catch (SQLException e) {
         e.printStackTrace();
     }
-    return tReqs();
+    return tReqs;
 }
 
 }
