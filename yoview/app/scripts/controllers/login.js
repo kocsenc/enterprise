385680369ab15e02078e59916a875373b9be15ab
kocsen <kocsenc@gmail.com>
2014-11-23 10:09:47 -0500

    Created template for login route: view & controller

---
+++ yoview/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name paybookApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the paybookApp
+ */
+angular.module('paybookApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
