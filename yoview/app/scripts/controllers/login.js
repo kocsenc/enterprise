82fb135ee1fcd0b2cf5c6de180594500596045d4
kocsen <kocsenc@gmail.com>
2014-11-23 10:36:26 -0500

    - basic login stuff

--- yoview/app/scripts/controllers/login.js
+++ yoview/app/scripts/controllers/login.js
@@ -8,10 +8,28 @@
  * Controller of the paybookApp
  */
 angular.module('paybookApp')
-  .controller('LoginCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('LoginCtrl', function ($scope, $http, $window) {
+
+
+    $scope.loginForm = {
+      email: "",
+      password: ""
+    };
+
+    $scope.pushLogin = function () {
+
+      $http
+        .post('/api/login', $scope.loginForm)
+        .success(function (data, status, headers, config) {
+          $window.sessionStorage.token = data.token;
+          $scope.message = 'Welcome';
+        })
+        .error(function (data, status, headers, config) {
+          // Erase the token if the user fails to log in
+          delete $window.sessionStorage.token;
+
+          // Handle login errors here
+          $scope.message = 'Error: Invalid user or password';
+        });
+    }
   });
