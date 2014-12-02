4ac558e0beee4f06daf1fcf54e8ed2e66d53f3cb
kocsen <kocsenc@gmail.com>
2014-12-02 17:14:00 -0500

    load test

--- test/requestLoadTest.py
+++ test/requestLoadTest.py
@@ -10,13 +10,44 @@ Paybook has to be running locally for this to work, or change URL variable
 """
 
 import requests
+import grequests
+
+import unittest
 
 
-def main():
-	print("yolo")
+class LoadTestPayBook(unittest.TestCase):
+        
+    def test_load(self):
+        """
+        Load Tests Paybook 
+        Creates requests synchronously and then asynchronously
+        """
+        URL = "http://vm343a.se.rit.edu/api/user/"
+        USER_COUNT = 15
+        NUM_REQS = 15 * 50
 
-	pass
+        url_ary = []
 
-if __name__ == "__main__":
-	main()
+        # Send requests synchronously
+        for i in range(NUM_REQS):
+            print("Issuing request #" + str(i+1))
+
+            user_num = i%USER_COUNT
+
+            url_i = URL + str(i) + "/getuser"
+            url_ary.append(url_i)
+            r = requests.get(url_i)
+            self.assertEqual(r.status_code, 200)
+
+
+        # Send requests all at the same time
+        print("Sending " +  str(NUM_REQS) + "requests at the same time")
+        rs = (grequests.get(u) for u in url_ary)
+        resary = grequests.map(rs)
+        for response in resary:
+            self.assertEqual(response.status_code, 200)
+
+    
+if __name__ == '__main__':
+    unittest.main()
 
